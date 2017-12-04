8db2d4bd911ef915325ba52e3c2ee0260f86477d
Luka Zdilar <luka-zdilar@uiowa.edu>
2017-12-04 13:57:12 -0600

    Add try with netflix data

--- test.py
+++ test.py
@@ -2,17 +2,26 @@ from surprise import SVD
 from surprise import Dataset, Reader
 from surprise import evaluate, print_perf
 
-reader_params=dict(line_format='user item rating timestamp',
+reader_params=dict(line_format='item rating user',
                    rating_scale=(1, 5),
                    sep='\t')
 reader = Reader(**reader_params)
 
-data = Dataset.load_builtin('ml-100k')
-data = Dataset.load_from_file(file_path='/Shared/bdagroup7/download/ml-100k/u.data', reader=reader)
-data.split(n_folds=3)
+data = Dataset.load_from_file(file_path='/Shared/bdagroup7/download/ratings.csv', reader=reader)
+training_set = data.build_full_trainset()
+test_set = training_set.build_testset()
+#data.split(n_folds=3)
 
 algo = SVD()
 
-perf = evaluate(algo, data, measures=['RMSE', 'MAE'])
+#perf = evaluate(algo, data, measures=['RMSE', 'MAE'])
 
-print_perf(perf)
+#print_perf(perf)
+
+algo.train(training_set)
+
+predctions = algo.test(test_set)
+
+# TODO: Ensemble
+
+surprise.accuracy.rmse(predictions, verbose=True)
